3beaab30d0ed929965947e57efa3a7e7024659f9
ZhiHeng Zhong <zhongzhiheng@sunlands.com>
2019-12-24 14:09:53 +0800

    fix

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const serve = require('koa-static');
 const appOne = new Koa();
-// const appTwo = new Koa();
+
 const routerOne = new Router();
-// const routerTwo = new Router();
+
 
 
 const serverBundle = require(path.resolve(__dirname, './dist/vue-ssr-server-bundle.json'));
@@ -19,9 +19,9 @@ const renderer = require('vue-server-renderer').createBundleRenderer(serverBundl
   clientManifest: clientManifest
 });
 
-// 后端Server
+
 routerOne.get('/index', async (ctx, next) => {
-  const context = {//这里的参数现在还没用，但这个对象还是得用，要做renderToString的参数
+  const context = {
     url:ctx.req.url
   }
   try {
@@ -54,21 +54,3 @@ appOne.use(routerOne.routes())
 appOne.listen(3001, () => {
   console.log('服务器端渲染地址： http://localhost:3001');
 });
-
-
-// // 前端Server
-// routerTwo.get('/index', (ctx, next) => {
-//   let html = fs.readFileSync(path.resolve(__dirname, '../dist/index.client.html'), 'utf-8');
-//   ctx.type = 'html';
-//   ctx.status = 200;
-//   ctx.body = html;
-// });
-
-// appTwo.use(serve(path.resolve(__dirname, '../dist')));
-
-// appTwo.use(routerTwo.routes())
-//   .use(routerTwo.allowedMethods()); //处理跨域问题
-
-// appTwo.listen(3002, () => {
-//   console.log('浏览器端渲染地址： http://localhost:3002');
-// });
